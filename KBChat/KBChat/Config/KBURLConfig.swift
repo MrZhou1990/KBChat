17cbd5606d9d803d194836804c7ab8c6567eb95b
周昊 <zhou-hao-neu@neusoft.com>
2021-08-02 09:38:46 +0800

    no message

---
+++ KBChat/KBChat/Config/KBURLConfig.swift
@@ -0,0 +1,14 @@
+//
+//  KBURLConfig.swift
+//  KBChat
+//
+//  Created by 周昊 on 2021/3/2.
+//
+
+import Foundation
+
+let DOMAINURL = "http://123.57.91.125:8082/push/"
+
+let iPhone11Token = "79f9a86f97481f0c07f8ff0001e378c4e8ad2281bc1528a2d2783a9f0cec58c5"
+let iPadProToken = "03854972fada97ddf2b2f61ea61adb1004f7af4a20a556b4b8948348ec5b7561"
+let iPhone6PlusToken = "ff024d2cefeccdc5e9bb77b0eab45a82e8015776f33d69d104983b90702b92a3"
